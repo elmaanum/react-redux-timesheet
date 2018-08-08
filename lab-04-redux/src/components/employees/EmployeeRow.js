eb657fb6d9cb15eb0e78c11c9f4c9bd7b55ae7b6
Erik Maanum <elmaanum@spscommerce.com>
2018-08-08 14:50:49 -0500

    I think I know redux now?!

--- lab-04-redux/src/components/employees/EmployeeRow.js
+++ lab-04-redux/src/components/employees/EmployeeRow.js
@@ -1,10 +1,39 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
+import { Button } from 'react-bootstrap';
 
 class EmployeeRow extends Component {
+  handleClick(employee) {
+    if (employee.deleted) {
+      employee.deleted = false;
+      this.props.actions
+        .restoreEmployee(employee)
+        .then(this.props.actions.listEmployees);
+    } else {
+      employee.deleted = true;
+      this.props.actions
+        .removeEmployee(employee)
+        .then(this.props.actions.listEmployees);
+    }
+  }
+  
   render() {
     const employee = this.props.employee;
+    let rowClass = '';
+    if (employee.deleted) {
+      rowClass = 'faded';
+    }
 
+    const button = (
+      <Button
+        onClick={() => {
+          this.handleClick(employee);
+        }}
+        bsStyle={employee.deleted ? 'success' : 'danger'}
+      >
+        {employee.deleted ? 'Restore' : 'Delete'}
+      </Button>
+    );
     return (
       <tr>
         <td>{employee.username}</td>
@@ -12,6 +41,7 @@ class EmployeeRow extends Component {
         <td>{employee.firstName}</td>
         <td>{employee.lastName}</td>
         <td>{employee.admin ? 'Yes' : 'No'}</td>
+        <td>{button}</td>
       </tr>
     );
   }
