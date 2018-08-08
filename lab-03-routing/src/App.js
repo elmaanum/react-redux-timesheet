9402048bed0f47e46b7504ffa63d1b9b3466884e
Erik Maanum <elmaanum@spscommerce.com>
2018-08-08 13:05:08 -0500

    We added some routes

--- lab-03-routing/src/App.js
+++ lab-03-routing/src/App.js
@@ -1,11 +1,26 @@
 import React, { Component } from 'react';
-
+import Projects from './components/projects/Projects'
+import Employees from './components/employees/Employees';
+import Timesheets from './components/timesheets/Timesheets';
+import { BrowserRouter, Route, Switch, Redirect } from 'react-router-dom';
 import './App.css';
 
 class App extends Component {
   // TODO - actually implement this for realz
   render() {
-    return <div />;
+    return (
+      <BrowserRouter>
+        <div className="App">
+        <Switch>
+          <Route path="/projects" component={Projects}/>
+          <Route exact path="/employees" component={Employees}/>
+          <Route path="/employees/:user_id/timesheets" component={Timesheets}/>
+          <Redirect to="/employees"/>
+        </Switch>
+
+        </div>
+      </BrowserRouter>
+    );
   }
 }
 
